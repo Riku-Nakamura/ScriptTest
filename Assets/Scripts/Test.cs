31464afcbc8beab766b1d950bb757764a05b5cbd
RikuNakamura <riku.nak@outlook.com>
2018-06-12 16:46:03 +0900

    TestとTest2にスクリプトを書きました。

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    private int[] array;
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("Hello unity again!!");
+        array = new int[5];
+        array = array.Select(_ => Random.Range(0, 500)).ToArray();
+        Debug.Log("\n-=-=ここから小さい順-=-=");
+        array.OrderBy(e => e).ToList().ForEach(e => Debug.Log(e));
+        Debug.Log("\n-=-=ここから大きい順-=-=");
+        array.OrderByDescending(e => e).ToList().ForEach(e => Debug.Log(e));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+    private string name;
+
+    public Boss(string name, int hp, int power, int mp)
+    {
+        this.hp = hp;
+        this.power = power;
+        this.mp = mp;
+        this.name = name;
+    }
+
+    public void Attack()
+    {
+        Debug.Log(this.name + "は" + this.power + "のダメージを与えた！");
+    }
+
+    public void Magic()
+    {
+        this.mp -= 5;
+        Debug.Log(
+            (this.mp > 0) ?
+                this.name + "は魔法攻撃をした。残りMPは" + this.mp +'。'
+                :
+                this.name + "はMPが足りないため魔法が使えない。"
+            );
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(this.name + "は" + damage + "のダメージを受けた！");
+        this.hp -= damage;
+    }
+
+}
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Boss boss = new Boss("ピカチュウ", 50000, 99999, 53);
+        boss.Magic();
+        Enumerable.Range(0, 10).ToList().ForEach(_ => boss.Magic());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
